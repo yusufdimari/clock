2e92dd03be024f513458c1bae6a10dd58845281f
yusufdimari <ysf.dimari.yd@gmail.com>
2021-05-10 10:45:51 +0100

    commit-1

---
+++ sandbox.js
@@ -0,0 +1,16 @@
+const clock=document.querySelector('.time');
+const tick=(()=>{
+    let now=new Date();
+    let h=now.getHours()-12;
+    let m=now.getMinutes();
+    let s=now.getSeconds();
+    let dat= now.toDateString();
+
+    const html=`<span class="content">${h}</span>:
+                <span class="content">${m}</span>:
+                <span class="content">${s}</span>
+                <p><span class="date">${dat}</span></p>`;
+    clock.innerHTML=html;
+});
+
+setInterval(tick, 1000);
