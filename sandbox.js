dd3bedb22671cbfcc720120eff5598f844fb8bfe
yusufdimari <ysf.dimari.yd@gmail.com>
2021-05-10 11:00:37 +0100

    commit-2

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,14 @@
 const clock=document.querySelector('.time');
 const tick=(()=>{
     let now=new Date();
-    let h=now.getHours()-12;
+    let h=now.getHours();
+    if(h>12){
+        h=h-12;
+    }
     let m=now.getMinutes();
     let s=now.getSeconds();
     let dat= now.toDateString();
-
+    
     const html=`<span class="content">${h}</span>:
                 <span class="content">${m}</span>:
                 <span class="content">${s}</span>
